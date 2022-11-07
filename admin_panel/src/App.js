40d1f4c9a64553c442bcd89404f22408f6457d68
Diego Germán López Cuello <diego.lopezc@sonda.com>
2022-11-07 14:22:14 -0300

    Task 27 - ReactJS project creation

---
+++ admin_panel/src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+
+        <h1>
+          QR Link
+        </h1>
+        <p>
+          Panel de administración
+        </p>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
