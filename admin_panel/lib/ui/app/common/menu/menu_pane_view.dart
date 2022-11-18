890d14d3f3afd7737483117495e7ac6a970b0faa
Bettina Carrizo <betticarrizo@gmail.com>
2022-11-17 22:48:23 -0300

    [TASK-82] Agregar producto - Mover archivos a common

---
+++ admin_panel/lib/ui/app/common/menu/menu_pane_view.dart
@@ -0,0 +1,54 @@
+import 'package:admin_panel/ui/app/common/menu/menu.dart';
+import 'package:admin_panel/ui/app/common/menu/widgets/main_nav_item.dart';
+import 'package:admin_panel/ui/app/constants/constants.dart';
+import 'package:flutter/material.dart';
+
+class MenuPaneView extends StatelessWidget {
+  const MenuPaneView({
+    this.selectedMenuItem = MenuOptions.addResource,
+    required this.mainNavAction,
+    Key? key,
+  }) : super(key: key);
+
+  final MenuOptions selectedMenuItem;
+  final Function mainNavAction;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.max,
+      children: [
+        // LOGO
+        Container(
+          height: 170,
+          decoration: BoxDecoration(
+              border: Border(
+                  bottom: BorderSide(color: AppColors.mustard_75, width: 4)),
+              image: DecorationImage(
+                scale: 1.4,
+                image: AssetImage(AppAssets.qrlinkLogo),
+              )),
+        ),
+        const SizedBox(height: 50),
+        // MAIN SECTION
+        Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: MenuOptions.values
+                .map(
+                  (menuOption) => Padding(
+                    padding: const EdgeInsets.only(bottom: 16.0),
+                    child: MainNavItem(
+                        label: menuOption.name,
+                        icon: menuOption.icon,
+                        isSelected: selectedMenuItem == menuOption,
+                        onTap: () => mainNavAction(menuOption)),
+                  ),
+                )
+                .toList(),
+          ),
+        ),
+      ],
+    );
+  }
+}
---
+++ admin_panel/lib/ui/app/constants/assets.dart
@@ -0,0 +1,4 @@
+class AppAssets {
+  static String qrlinkLogoAnim = 'assets/img/qr_logo_anim.json';
+  static String qrlinkLogo = 'assets/img/qr_logo.png';
+}
